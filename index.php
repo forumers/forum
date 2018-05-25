dff55599de0a819ec1db63fae7b4e3873a09082e
Andreas P <apapuig@gmail.com>
2018-05-24 21:32:55 -0400

    AAA

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/main.css" />
+</head>
+
+<body>
+    <header>
+        <div class="main-wrapper">
+            <ul>
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+            </ul>
+            <div class="nav-login">
+                <form>
+                    <input type="text" name="uid" placeholder="Username/E-mail">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <button type="submit" name="submit">Login</button>
+                </form>
+                <a href="register.php">Register</a>
+            </div>
+        </div>
+    </header>
+</body>
+
+</html>
