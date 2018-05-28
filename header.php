b1382ea1e0f053210867f12567ab1dba50d6c166
Andreas P <apapuig@gmail.com>
2018-05-27 23:08:09 -0400

    Deleted CSS reset code.

--- header.php
+++ header.php
@@ -21,14 +21,14 @@
                     if (isset($_SESSION['u_id'])) {
                         echo '<form action="./includes/logout.php" method="POST">
                                 <button type="submit" name="submit">Logout</button>
-                            </form>';
+                              </form>';
                     } else {
                         echo '<form action="./includes/login.php" method="POST">
-                        <input type="text" name="uid" placeholder="Username/Email">
-                        <input type="password" name="pwd" placeholder="Password">
-                        <button type="submit" name="submit">Login</button>
-                    </form>
-                    <a href="register.php">Register</a>';
+                                <input type="text" name="uid" placeholder="Username/Email">
+                                <input type="password" name="pwd" placeholder="Password">
+                                <button type="submit" name="submit">Login</button>
+                              </form>
+                        <a href="register.php">Register</a>';
                     }
                 ?>
             </div>
